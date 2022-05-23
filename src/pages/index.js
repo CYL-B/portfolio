6d506305ba2b3abbbfef7b83525946ff24c8af96
Camille Benhammadi <benhammadcamille@hotmail.fr>
2022-05-23 04:51:00 +0200

    useeffect

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,20 +56,23 @@ width:100vw;
 
 const IndexPage = () => {
 
-  useEffect = (() =>{
-    AOS.init({
-      // Global settings:
-      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-      initClassName: 'aos-init', // class applied after initialization
-      animatedClassName: 'aos-animate', // class applied on animation
-      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-    })
+  useEffect (() => {
+    const animate = () => {
+      AOS.init({
+        // Global settings:
+        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+      })
+    }
+    animate()
   }, [])
-  
+
 
 
   const ref1 = useRef();
@@ -108,8 +111,8 @@ const IndexPage = () => {
           handleClick4={() => handleClick4()}></SlideMenu>
 
 
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent:"center", height:"90%"}}>
-          <TitleWrapper  data="zoom-out-down" delay="50" title="Camille" size="xl" line="title" duration="500" margin="title"></TitleWrapper>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "90%" }}>
+          <TitleWrapper data="zoom-out-down" delay="50" title="Camille" size="xl" line="title" duration="500" margin="title"></TitleWrapper>
           <TitleWrapper margin="title" data="zoom-out-down" delay="500" title="Benhammadi" size="xl" duration="500"></TitleWrapper>
           <Subtitle data="fade-zoom-in" easing="ease-in-back" delay="1000" duration="1000"><HighLightWrapper>Developer</HighLightWrapper></Subtitle>
           <Button taille="normal" onClick={() => handleClick1()}>More</Button>
@@ -118,75 +121,75 @@ const IndexPage = () => {
 
       </Layout>
 
-      <Layout id="layout2" layout={{ width: "100vw", height: "100vh" , margin: "10px 0px 10px 0px"}}>
+      <Layout id="layout2" layout={{ width: "100vw", height: "100vh", margin: "10px 0px 10px 0px" }}>
         <div ref={ref1}></div>
         <TitleWrapper data="fade-right"
-    
-    easing="ease-in-out-quad" delay="50" margin="title" title="About me" size="l"></TitleWrapper>
+
+          easing="ease-in-out-quad" delay="50" margin="title" title="About me" size="l"></TitleWrapper>
         <DivAbout>
 
           <div data-aos="fade-up"
-    data-aos-offset="200"
-    data-aos-delay="50"
-    data-aos-duration="2000"
-    data-aos-easing="ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="false" style={{ width: "50%", display: "flex", justifyContent: "center" }}><Avatar ></Avatar></div>
+            data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="2000"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="false" style={{ width: "50%", display: "flex", justifyContent: "center" }}><Avatar ></Avatar></div>
           <div style={{ width: "50%", display: "flex", flexDirection: "column", alignContent: "center" }}><TitleWrapper font="zilla" size="s">From <HighLightWrapper>law</HighLightWrapper> to <HighLightWrapper>tech</HighLightWrapper></TitleWrapper>
             <TitleWrapper data="fade-left" duration="1500" delay="500" line="true" margin="desc" align="justify" font="zilla" size="xs">Hello !
-       I’m Camille, I'm a french jurist turned <HighLightWrapper>full stack developer</HighLightWrapper> with a keen interest for design, UX/UI and accessibility. In 2021, I joined a french bootcamp called <a style={{  textDecoration: "none"}}rel="noreferrer" target="_blank" href="https://www.lacapsule.academy/">"La Capsule"</a> to learn how to code with <HighLightWrapper>Javascript</HighLightWrapper> and worked as a teacher’s assistant for 2 months. 
-I would love to be part of a team where I can put my skills to good use and learn new ones. I'm based in <HighLightWrapper>Montréal</HighLightWrapper> ,Canada.</TitleWrapper></div></DivAbout>
+              I’m Camille, I'm a french jurist turned <HighLightWrapper>full stack developer</HighLightWrapper> with a keen interest for design, UX/UI and accessibility. In 2021, I joined a french bootcamp called <a style={{ textDecoration: "none" }} rel="noreferrer" target="_blank" href="https://www.lacapsule.academy/">"La Capsule"</a> to learn how to code with <HighLightWrapper>Javascript</HighLightWrapper> and worked as a teacher’s assistant for 2 months.
+              I would love to be part of a team where I can put my skills to good use and learn new ones. I'm based in <HighLightWrapper>Montréal</HighLightWrapper> ,Canada.</TitleWrapper></div></DivAbout>
 
 
       </Layout>
-      <Layout  id="layout3" layout={{margin: "10px 0px 10px 0px", width: "100vw", height: "100vh" }}>
+      <Layout id="layout3" layout={{ margin: "10px 0px 10px 0px", width: "100vw", height: "100vh" }}>
         <div ref={ref2}></div>
-      <TitleWrapper data="fade-right"
-    
-    easing="ease-in-out-quad" delay="300" margin="title" size="l">Portfolio</TitleWrapper>
-        <div style={{display:"flex", alignItems:"center", width:"100%", height:"100%"}}>
-        
-    <TitleWrapper margin="title" data="fade-right"
-   
-    easing="ease-in-out-quad" delay="600"  font="zilla" size="m" >Here are the projects that I have worked on/have been working on as a fullstack developer.</TitleWrapper>
-    <div style={{padding:"10px", minWidth:"55vw", maxHeight:"70vh", overflowY:"auto", borderLeft:"5px solid rgba(246, 205, 139, 0.7)", alignSelf:"start"}}>
-       <Card data="fade-right" delay="500" duration="1000" youtube href="https://youtu.be/Fx4hOgKBU04" href2="https://github.com/CYL-B/Kiosk-front" source={Kiosk} title="KIOSK" techno="#ReactNative #ReactnativeElements #MongoDB#NodeJs" margin>KIOSK is a marketplace for professionals looking for ethical and eco-friendly services and products for their business.<br></br>
-          This is my coding bootcamp final project. I worked with a team of 3 people to turn it into a mobile application in 2 weeks. KIOSK was built with React Native.</Card>
+        <TitleWrapper data="fade-right"
 
-          <Card link href3="https://calm-dawn-80536.herokuapp.com/" title="Droit(s) Communs" source={Droits} href2="https://github.com/CYL-B/droits-communs.git" techno="#React #MUI #MongoDB #Nodejs #mailchimp #Leaflet" margin>Droit(s) commun(s) is a personal project that I designed and am currently developing which aims to promote legal design. My goal was to create a media to make law accessible to people with no academic background in that field. 
+          easing="ease-in-out-quad" delay="300" margin="title" size="l">Portfolio</TitleWrapper>
+        <div style={{ display: "flex", alignItems: "center", width: "100%", height: "100%" }}>
 
-I'm still working on it but you can check it out ! 
- </Card>
- </div>
- </div>
+          <TitleWrapper margin="title" data="fade-right"
+
+            easing="ease-in-out-quad" delay="600" font="zilla" size="m" >Here are the projects that I have worked on/have been working on as a fullstack developer.</TitleWrapper>
+          <div style={{ padding: "10px", minWidth: "55vw", maxHeight: "70vh", overflowY: "auto", borderLeft: "5px solid rgba(246, 205, 139, 0.7)", alignSelf: "start" }}>
+            <Card data="fade-right" delay="500" duration="1000" youtube href="https://youtu.be/Fx4hOgKBU04" href2="https://github.com/CYL-B/Kiosk-front" source={Kiosk} title="KIOSK" techno="#ReactNative #ReactnativeElements #MongoDB#NodeJs" margin>KIOSK is a marketplace for professionals looking for ethical and eco-friendly services and products for their business.<br></br>
+              This is my coding bootcamp final project. I worked with a team of 3 people to turn it into a mobile application in 2 weeks. KIOSK was built with React Native.</Card>
+
+            <Card link href3="https://calm-dawn-80536.herokuapp.com/" title="Droit(s) Communs" source={Droits} href2="https://github.com/CYL-B/droits-communs.git" techno="#React #MUI #MongoDB #Nodejs #mailchimp #Leaflet" margin>Droit(s) commun(s) is a personal project that I designed and am currently developing which aims to promote legal design. My goal was to create a media to make law accessible to people with no academic background in that field.
+
+              I'm still working on it but you can check it out !
+            </Card>
+          </div>
+        </div>
       </Layout>
 
       <Layout id="layout4" layout={{ width: "100vw", height: "100vh", margin: "10px 0px 10px 0px" }}>
         <div ref={ref3}></div>
         <TitleWrapper data="fade-right"
-    
-    easing="ease-in-out-quad" delay="300" margin="title" title="Technologies" size="l"></TitleWrapper>
+
+          easing="ease-in-out-quad" delay="300" margin="title" title="Technologies" size="l"></TitleWrapper>
         <div style={{ display: "flex", justifyContent: "space-evenly", maxHeight: "70%", width: "100%" }}>
           <div data-aos="fade-zoom-in"
-     data-aos-easing="ease-in-back"
-     data-aos-delay="500"
-     data-aos-duration="700"
-     data-aos-offset="0" style={{ maxHeight: "100%", overflowY: "auto" }}><TitleWrapper font="zilla" size="s" align="centre">Front-end</TitleWrapper>
+            data-aos-easing="ease-in-back"
+            data-aos-delay="500"
+            data-aos-duration="700"
+            data-aos-offset="0" style={{ maxHeight: "100%", overflowY: "auto" }}><TitleWrapper font="zilla" size="s" align="centre">Front-end</TitleWrapper>
             <CardDivider></CardDivider>
             <Techno type="front"></Techno></div>
 
           <div data-aos="fade-zoom-in"
-     data-aos-easing="ease-in-back"
-     data-aos-delay="900"
-     data-aos-duration="700"
-     data-aos-offset="0"><TitleWrapper font="zilla" size="s" align="centre">Back-end</TitleWrapper>
+            data-aos-easing="ease-in-back"
+            data-aos-delay="900"
+            data-aos-duration="700"
+            data-aos-offset="0"><TitleWrapper font="zilla" size="s" align="centre">Back-end</TitleWrapper>
             <CardDivider></CardDivider>
             <Techno type="back"></Techno></div>
           <div data-aos="fade-zoom-in"
-     data-aos-easing="ease-in-back"
-     data-aos-delay="1300"
-     data-aos-duration="700"
-     data-aos-offset="0"><TitleWrapper font="zilla" size="s" align="centre">Design & more</TitleWrapper>
+            data-aos-easing="ease-in-back"
+            data-aos-delay="1300"
+            data-aos-duration="700"
+            data-aos-offset="0"><TitleWrapper font="zilla" size="s" align="centre">Design & more</TitleWrapper>
             <CardDivider></CardDivider>
             <Techno type="design"></Techno></div></div>
       </Layout>
@@ -195,15 +198,15 @@ I'm still working on it but you can check it out !
         <Layout id="layout5" layout={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "space-around", margin: "10px 0px 10px 0px" }}>
           <DivContact ref={ref4}>
             <TitleWrapper data="fade-right"
-    
-    easing="ease-in-out-quad" delay="300" margin="title" title="Contact" size="l"></TitleWrapper>
+
+              easing="ease-in-out-quad" delay="300" margin="title" title="Contact" size="l"></TitleWrapper>
             <TitleWrapper data="fade-right"
-    
-    easing="ease-in-out-quad" delay="600"  font="zilla" size="xs"><HighLightWrapper>Let's work together !</HighLightWrapper></TitleWrapper>
+
+              easing="ease-in-out-quad" delay="600" font="zilla" size="xs"><HighLightWrapper>Let's work together !</HighLightWrapper></TitleWrapper>
             <CardDivider></CardDivider>
             <TitleWrapper data="fade-right"
-    
-    easing="ease-in-out-quad" delay="900" font="zilla" size="xs">Email : benhammadcamille@hotmail.fr</TitleWrapper>
+
+              easing="ease-in-out-quad" delay="900" font="zilla" size="xs">Email : benhammadcamille@hotmail.fr</TitleWrapper>
           </DivContact>
           <ContactForm data="fade-down-left" duration="2000" easing="ease-in-out-cubic"></ContactForm>
 
