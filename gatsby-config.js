091aed2428a39abf7a8fa300b8438dab7c00a74a
Camille CYL-B <benhammadcamille@hotmail.fr>
2025-07-04 01:04:14 +0200

    chore : updated dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,14 @@
-const path = require(`path`)
+const path = require(`path`);
 
 module.exports = {
   siteMetadata: {
     title: `Camille Fullstack Developer`,
     description: `Hi ! I'm Camille, I'm a web developer and this is where I showcase what I can do.`,
     siteUrl: `https://www.camillebenhammadiportfolio.com/`,
-    image: `https://imagizer.imageshack.com/img924/7010/r63NBo.jpg`
+    image: `https://imagizer.imageshack.com/img924/7010/r63NBo.jpg`,
   },
-  plugins: ["gatsby-plugin-styled-components",
+  plugins: [
+    "gatsby-plugin-styled-components",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-plugin-netlify",
@@ -31,36 +32,43 @@ module.exports = {
     {
       resolve: `gatsby-plugin-remote-images`,
       options: {
-        nodeType: 'MongodbPortfolioProfile',
-        imagePath: 'https://imagizer.imageshack.com/img924/7010/r63NBo.jpg',
-      }
+        nodeType: "MongodbPortfolioProfile",
+        imagePath: "https://imagizer.imageshack.com/img924/7010/r63NBo.jpg",
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-         icon: `src/images/favicon.png`,
-         cache_busting_mode: `none`
-      }
+        name: `Camille Fullstack Developer`,
+        short_name: `Camille Dev`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#F4ECE4`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`,
+        cache_busting_mode: `none`,
+      },
     },
     {
       resolve: `gatsby-source-mongodb`,
-      options: { 
-        dbName: `Portfolio`, 
-      auth: {
-        user: 'Camdev',
-        password: 'E16Rrx0HRym5TVCY'
-    }, 
-    collection: [`Projects`, `Profile`],
-    server: {
-        address: 'cluster0-shard-00-01.d5pln.mongodb.net',
-        port: 27017
+      options: {
+        dbName: `Portfolio`,
+        auth: {
+          user: "Camdev",
+          password: "E16Rrx0HRym5TVCY",
+        },
+        collection: [`Projects`, `Profile`],
+        server: {
+          address: "cluster0-shard-00-01.d5pln.mongodb.net",
+          port: 27017,
+        },
+        extraParams: {
+          replicaSet: "cluster0-shard-01",
+          ssl: true,
+          authSource: "admin",
+          retryWrites: true,
+        },
+      },
     },
-    extraParams:{
-      replicaSet: 'cluster0-shard-01',
-            ssl: true,
-            authSource: 'admin',
-            retryWrites: true
-    } },
-    }
-  ]
+  ],
 };
